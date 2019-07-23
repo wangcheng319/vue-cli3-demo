eb95b9e675611c4f7b0bd3617955643910f38893
王成 <274281610@qq.com>
2019-07-23 15:32:33 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import i18n from './config/lang'
+import filters from './filters'
+import 'amfe-flexible'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Toast from './components/toast/index'
+Vue.use(Toast);
+
+// 消除 click 移动浏览器300ms延迟
+import attachFastClick from 'fastclick'
+attachFastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+
+
+Vue.use(VueAxios, axios)
+
+axios.defaults.baseURL = '/api/'
+// 如果是非线上环境，加载 VConsole（移动端适用）
+// if (process.env.NODE_ENV !== 'production') {
+//     var VConsole = require('vconsole/dist/vconsole.min.js');
+//     var vConsole = new VConsole();
+// }
+
+Vue.config.productionTip = false
+
+// 全局过滤器
+Object.keys(filters).forEach(filterName => {
+    Vue.filter(filterName, filters[filterName])
+})
+
+/* eslint-disable no-new */
+new Vue({
+    router,
+    store,
+    i18n,
+    render: h => h(App)
+}).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,63 @@
+// vue.config.js
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+module.exports = {
+    // 使用运行时编译器的 Vue 构建版本
+    runtimeCompiler: true,
+
+    // 开启生产环境SourceMap
+    productionSourceMap: false,
+
+    // 关闭ESLint
+    lintOnSave: false,
+
+    devServer: {
+        open: false,        // 是否自动打开浏览器页面
+        host: '0.0.0.0',    // 指定使用一个 host，默认是 localhost
+        port: 8080,         // 端口地址
+        https: false,       // 使用https提供服务
+        // 设置代理，此处应该配置为开发服务器的后台地址
+        proxy: {
+            '/api': {
+                target: 'https://wanandroid.com', //对应的接口
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    },
+
+    chainWebpack: (config) => {
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('assets', resolve('src/assets'))
+            .set('components', resolve('src/components'))
+    },
+
+    configureWebpack: config => {
+        // 生产环境打包分析体积
+        if (process.env.NODE_ENV === 'production') {
+            return {
+                plugins: [
+                    new BundleAnalyzerPlugin()
+                ]
+            }
+        }
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                data: `
+                @import "@/style/mixin.scss";
+                @import "@/style/_var.scss";
+                `
+            }
+        }
+    },
+};
